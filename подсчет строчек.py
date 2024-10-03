d72a5cf32f167d1b872b1ad7b7206b148f626e7b
cartoonboxx <96394262+cartoonboxx@users.noreply.github.com>
2024-10-03 23:11:42 +0300

    Release Commit

--- подсчет строчек.py
+++
@@ -1,9 +0,0 @@
-import os
-
-total = [i for i in os.listdir() if '.py' in i]
-strings = 0
-for i in total:
-    with open(i, encoding='utf-8') as file:
-        strings += len(file.readlines())
-
-print(strings)
